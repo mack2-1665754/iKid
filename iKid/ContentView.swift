906aaaed7c98bdae7f4837930e920c72bb3ab898
mackenziehutchison <mack2@uw.edu>
2021-05-06 15:16:32 -0700

    fix

--- iKid/ContentView.swift
+++ iKid/ContentView.swift
@@ -6,8 +6,28 @@
 //
 
 import SwiftUI
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+// A View wrapper to make the modifier easier to use
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
 
 struct ContentView: View {
+    @State private var orientation = UIDeviceOrientation.unknown
+
     @EnvironmentObject var jokeItemStorage: JokeItemStorage
     var body: some View {
         NavigationView {
@@ -25,6 +45,8 @@ struct ContentView: View {
                     
                 }
             }
+        }.onRotate { newOrientation in
+            orientation = newOrientation
         }
     }
     
